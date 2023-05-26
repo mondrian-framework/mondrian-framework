fd313a136667a19415d74be9e51bae96b6126b48
Edoardo Barbieri <edoardo@twinlogix.com>
2023-05-26 14:37:20 +0200

    chore: finish timestamp implementation

--- packages/model/src/custom/timestamp.ts
+++ packages/model/src/custom/timestamp.ts
@@ -5,9 +5,29 @@ export function timestamp(opts?: TimestampType['opts']): TimestampType {
   return custom(
     {
       name: 'timestamp',
-      decode: (input) => {
+      decode: (input, settings) => {
         if (typeof input !== 'number') {
           return { pass: false, errors: [{ path: '', value: input, error: 'Unix time expected (ms)' }] }
+        }
+        if (input > 864000000000000 || input < -864000000000000) {
+          return {
+            pass: false,
+            errors: [
+              { path: '', value: input, error: `Timestamp must be between -864000000000000 and 864000000000000` },
+            ],
+          }
+        }
+        if (settings?.max != null && input > settings.max.getTime()) {
+          return {
+            pass: false,
+            errors: [{ path: '', value: input, error: `Timestamp must be maximum ${settings.max.toISOString()}` }],
+          }
+        }
+        if (settings?.min != null && input < settings.min.getTime()) {
+          return {
+            pass: false,
+            errors: [{ path: '', value: input, error: `Timestamp must be minimum ${settings.min.toISOString()}` }],
+          }
         }
         return { pass: true, value: new Date(input) }
       },

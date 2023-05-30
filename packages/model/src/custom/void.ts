1d6978e06c00dced033f257d68114b94dd808733
Edoardo Barbieri <edoardo@twinlogix.com>
2023-05-30 12:30:33 +0200

    fix: void type

--- packages/model/src/custom/void.ts
+++ packages/model/src/custom/void.ts
@@ -1,25 +1,24 @@
 import { success } from '../result'
-import { CustomType, custom, literal } from '../type-system'
+import { RootCustomType, literal } from '../type-system'
 
-//TODO: make it root
 const VoidEncodeType = literal(null).optional()
 type VoidEncodeType = typeof VoidEncodeType
-export type VoidType = CustomType<void, VoidEncodeType, {}>
+export type VoidType = RootCustomType<void, VoidEncodeType, {}>
 export function voidType(opts?: VoidType['opts']): VoidType {
-  return custom(
-    {
-      name: 'void',
-      encodedType: VoidEncodeType,
-      decode: () => {
-        return success<void>(undefined)
-      },
-      encode: () => {
-        return undefined
-      },
-      validate() {
-        return success<void>(undefined)
-      },
+  return {
+    kind: 'custom',
+    name: 'void',
+    type: undefined as void,
+    encodedType: VoidEncodeType,
+    decode: () => {
+      return success<void>(undefined)
+    },
+    encode: () => {
+      return undefined
+    },
+    validate() {
+      return success<void>(undefined)
     },
     opts,
-  )
+  }
 }

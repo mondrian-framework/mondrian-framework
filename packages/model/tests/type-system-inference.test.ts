4f85f67dc7abff3a635165671b682eda89fcc17d
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-07-06 10:36:53 +0200

    chore: add test for type inference of more basic types

--- packages/model/tests/type-system-inference.test.ts
+++ packages/model/tests/type-system-inference.test.ts
@@ -3,6 +3,30 @@ import { nonEmptyArray } from './generator-utils'
 import { test, fc as gen } from '@fast-check/vitest'
 import { expect, expectTypeOf, describe } from 'vitest'
 
+describe('number', () => {
+  test('number type is inferred to be a number', () => {
+    type Age = m.Infer<typeof age>
+    const age = m.number()
+    expectTypeOf<Age>().toEqualTypeOf<number>()
+  })
+})
+
+describe('string', () => {
+  test('string type is inferred to be a string', () => {
+    type Username = m.Infer<typeof username>
+    const username = m.string()
+    expectTypeOf<Username>().toEqualTypeOf<string>()
+  })
+})
+
+describe('boolean', () => {
+  test('boolean type is inferred to be a boolean', () => {
+    type AdminFlag = m.Infer<typeof adminFlag>
+    const adminFlag = m.boolean()
+    expectTypeOf<AdminFlag>().toEqualTypeOf<boolean>()
+  })
+})
+
 describe('enumeration', () => {
   test('enum type is inferred to be a union of literal strings', () => {
     type TestEnum = m.Infer<typeof testEnum>

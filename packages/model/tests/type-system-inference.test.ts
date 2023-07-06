45ec4709bc670737279decb8a30cd4275a8c217f
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-07-06 12:34:14 +0200

    chore: add a better name to test suites

--- packages/model/tests/type-system-inference.test.ts
+++ packages/model/tests/type-system-inference.test.ts
@@ -3,44 +3,44 @@ import { nonEmptyArray } from './generator-utils'
 import { test, fc as gen } from '@fast-check/vitest'
 import { expect, expectTypeOf, describe } from 'vitest'
 
-describe('number', () => {
-  test('number type is inferred to be a number', () => {
+describe('NumberType', () => {
+  test('a NumberType is inferred to be a number', () => {
     type Age = m.Infer<typeof age>
     const age = m.number()
     expectTypeOf<Age>().toEqualTypeOf<number>()
   })
 })
 
-describe('string', () => {
-  test('string type is inferred to be a string', () => {
+describe('StringType', () => {
+  test('a StringType is inferred to be a string', () => {
     type Username = m.Infer<typeof username>
     const username = m.string()
     expectTypeOf<Username>().toEqualTypeOf<string>()
   })
 })
 
-describe('boolean', () => {
-  test('boolean type is inferred to be a boolean', () => {
+describe('BooleanType', () => {
+  test('a BooleanType is inferred to be a boolean', () => {
     type AdminFlag = m.Infer<typeof adminFlag>
     const adminFlag = m.boolean()
     expectTypeOf<AdminFlag>().toEqualTypeOf<boolean>()
   })
 })
 
-describe('literal', () => {
-  test('string literal is inferred to be the corresponding string literal', () => {
+describe('LiteralType', () => {
+  test('a LiteralType with a string literal is inferred to be the corresponding string literal', () => {
     type Literal = m.Infer<typeof literal>
     const literal = m.literal('literal')
     expectTypeOf<Literal>().toEqualTypeOf<'literal'>()
   })
 
-  test('number literal is inferred to be the corresponding number literal', () => {
+  test('a LiteralType with a number literal is inferred to be the corresponding number literal', () => {
     type Literal = m.Infer<typeof literal>
     const literal = m.literal(1)
     expectTypeOf<Literal>().toEqualTypeOf<1>()
   })
 
-  test('boolean literal is inferred to be the corresponding boolean literal', () => {
+  test('a LiteralType with a boolean literal is inferred to be the corresponding boolean literal', () => {
     type LiteralTrue = m.Infer<typeof literalTrue>
     const literalTrue = m.literal(true)
     expectTypeOf<LiteralTrue>().toEqualTypeOf<true>()
@@ -50,15 +50,15 @@ describe('literal', () => {
     expectTypeOf<LiteralFalse>().toEqualTypeOf<false>()
   })
 
-  test('null literal is inferred to be the null literal', () => {
+  test('a LiteralType with a null literal is inferred to be the null literal', () => {
     type Literal = m.Infer<typeof literal>
     const literal = m.literal(null)
     expectTypeOf<Literal>().toEqualTypeOf<null>()
   })
 })
 
-describe('object', () => {
-  test('an object type is inferred to be an object with the corresponding fields', () => {
+describe('ObjectType', () => {
+  test('an ObjectType is inferred to be an object with the corresponding fields', () => {
     type User = m.Infer<typeof user>
     const user = m.object(
       {
@@ -85,21 +85,21 @@ describe('object', () => {
   })
 })
 
-describe('enumeration', () => {
-  test('enum type is inferred to be a union of literal strings', () => {
+describe('EnumerationType', () => {
+  test('an EnumerationType is inferred to be a union of literal strings', () => {
     type TestEnum = m.Infer<typeof testEnum>
     const testEnum = m.enum(['type1', 'type2', 'type3'])
     expectTypeOf<TestEnum>().toEqualTypeOf<'type1' | 'type2' | 'type3'>()
   })
 
-  test('enum type with a single case is inferred to be a literal string', () => {
+  test('an EnumerationType with a single case is inferred to be a literal string', () => {
     type TestEnum = m.Infer<typeof testEnum>
     const testEnum = m.enum(['type'])
     expectTypeOf<TestEnum>().toEqualTypeOf<'type'>()
   })
 
   test.prop([nonEmptyArray(gen.string())])(
-    'enum function generates an enumeration with the given fields',
+    'the enum function generates an EnumerationType with the given fields',
     (strings) => {
       const testEnum = m.enum(strings)
       expect(testEnum.values).toEqual(strings)
@@ -107,8 +107,8 @@ describe('enumeration', () => {
   )
 })
 
-describe('array', () => {
-  test('the array decorator can turn a type into an array of that type', () => {
+describe('ArrayDecorator', () => {
+  test('ArrayDecorator can turn a type into an array of that type', () => {
     type StringArray = m.Infer<typeof stringArray>
     const stringArray = m.string().array({
       name: 'a list of at most 3 strings',
@@ -124,14 +124,14 @@ describe('array', () => {
   })
 })
 
-describe('optional', () => {
-  test('the optional decorator can turn a type into an optional version', () => {
+describe('OptionalDecorator', () => {
+  test('OptionalDecorator can turn a type into an optional version', () => {
     type OptionalString = m.Infer<typeof optionalString>
     const optionalString = m.string().optional()
     expectTypeOf<OptionalString>().toEqualTypeOf<undefined | string>()
   })
 
-  test('the optional decorator is idempotent with regard to type inference', () => {
+  test('OptionalDecorator is idempotent with regard to type inference', () => {
     type One = m.Infer<typeof one>
     type Other = m.Infer<typeof other>
     const one = m.optional(m.optional(m.string()))
@@ -140,14 +140,14 @@ describe('optional', () => {
   })
 })
 
-describe('nullable', () => {
-  test('the nullable decorator can turn a type into a nullable version', () => {
+describe('NullableDecorator', () => {
+  test('NullableDecorator can turn a type into a nullable version', () => {
     type NullableString = m.Infer<typeof nullableString>
     const nullableString = m.string().nullable()
     expectTypeOf<NullableString>().toEqualTypeOf<null | string>()
   })
 
-  test('the nullable decorator is idempotent with regard to type inference', () => {
+  test('NullableDecorator is idempotent with regard to type inference', () => {
     type One = m.Infer<typeof one>
     type Other = m.Infer<typeof other>
     const one = m.nullable(m.nullable(m.string()))

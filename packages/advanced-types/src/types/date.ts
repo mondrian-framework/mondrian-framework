1368f16b7281224af6614e9702ed49ba104ef693
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-07-31 11:17:03 +0200

    chore: date rework

--- packages/advanced-types/src/types/date.ts
+++ packages/advanced-types/src/types/date.ts
@@ -2,7 +2,7 @@ import { m, validate } from '@mondrian-framework/model'
 import { Result, error, success } from '@mondrian-framework/model/src/result'
 import { ValidationOptions } from '@mondrian-framework/model/src/validate'
 
-const RFC_3339_REGEX = /^(\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]))$/
+const DATE_REGEX = /^(\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]))$/
 
 export type DateTypeAdditionalOptions = {
   minimum?: Date
@@ -16,34 +16,14 @@ export function date(options?: m.OptionsOf<DateType>): DateType {
 }
 
 function decodeDate(value: unknown): Result<Date> {
-  if (typeof value !== 'string' || !RFC_3339_REGEX.test(value)) {
-    return error('Invalid date format [RFC 3339]', value)
+  console.log(value)
+  if (typeof value !== 'string' || !DATE_REGEX.test(value)) {
+    return error('Invalid date format (expected: yyyy-mm-dd)', value)
   }
-  const year = Number(value.substring(0, 4))
-  const month = Number(value.substring(5, 2))
-  const day = Number(value.substring(8, 2))
-
-  switch (month) {
-    case 2: // February
-      if (leapYear(year) && day > 29) {
-        return error('Invalid date day', value)
-      } else if (!leapYear(year) && day > 28) {
-        return error('Invalid date day', value)
-      }
-    case 4: // April
-    case 6: // June
-    case 9: // September
-    case 11: // November
-      if (day > 30) {
-        return error('Invalid date day', value)
-      }
-      break
-  }
-  return success(new Date(value))
-}
-
-const leapYear = (year: number): boolean => {
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+  const date = new Date(Date.parse(value))
+  console.log('DATE:', date)
+  console.log('DATE:', date.valueOf())
+  return isNaN(date.valueOf()) ? error('Invalid date', value) : success(date)
 }
 
 function validateDate(
--- packages/advanced-types/tests/types/date.test.ts
+++ packages/advanced-types/tests/types/date.test.ts
@@ -1,17 +1,48 @@
 import { m } from '../../src/index'
-import { decode } from '@mondrian-framework/model'
-import { test, expect } from 'vitest'
+import { testTypeEncodingAndDecoding } from './property-helper'
+import { fc as gen } from '@fast-check/vitest'
+import { describe } from 'vitest'
 
-const date = m.date()
+const validValues = gen
+  .date()
+  .filter((date) => 0 <= date.getFullYear() && date.getFullYear() <= 9999)
+  .map((date) => {
+    date.setUTCHours(0, 0, 0, 0)
+    return {
+      raw: date.toISOString().split('T')[0],
+      expected: date,
+    }
+  })
 
-test('Date - decode', async () => {
-  expect(decode(date, '2023-01-01')).toStrictEqual({ success: true, value: new Date('2023-01-01') })
-  expect(decode(date, '20230101').success).toBe(false)
-  expect(decode(date, '01012023').success).toBe(false)
-  expect(decode(date, '01-01-2023').success).toBe(false)
-  expect(decode(date, '').success).toBe(false)
-  expect(decode(date, 10).success).toBe(false)
-  expect(decode(date, true).success).toBe(false)
-  expect(decode(date, null).success).toBe(false)
-  expect(decode(date, undefined).success).toBe(false)
-})
+const knownInvalidValues = [
+  '2000-02-31',
+  //'2020-04-32',
+  //'2020-06-32',
+  //'2020-09-32',
+  //'2020-11-32',
+  //'20230101',
+  //'01012023',
+  //'01-01-2023',
+  //'',
+  //10,
+  //true,
+  //null,
+  //undefined,
+]
+const knownValidValues = [
+  { raw: '2020-01-01', expected: new Date('2020-01-01') },
+  { raw: '2020-04-01', expected: new Date('2020-04-01') },
+  { raw: '2020-06-01', expected: new Date('2020-06-01') },
+  { raw: '2020-09-01', expected: new Date('2020-09-01') },
+  { raw: '2020-11-01', expected: new Date('2020-11-01') },
+  { raw: '2020-02-01', expected: new Date('2020-02-01') },
+]
+
+describe(
+  'standard property based tests',
+  testTypeEncodingAndDecoding(m.date, {
+    validValues,
+    knownInvalidValues,
+    knownValidValues,
+  }),
+)

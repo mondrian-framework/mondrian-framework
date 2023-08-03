6a0b8c65a856a3fed3cabc142f688c921a41dc46
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-08-03 09:38:54 +0200

    test: add encoder test stubs

---
+++ packages/model/tests/encoder.test.ts
@@ -0,0 +1,15 @@
+import { encoder, types } from '../src'
+import { test } from '@fast-check/vitest'
+import { describe, expect } from 'vitest'
+
+describe('encoder.encodeWithoutValidation', () => {
+  test('can encode a number type as a number', () => {
+    expect.fail('TODO')
+  })
+})
+
+describe('encoder.encode', () => {
+  test('performs validation', () => {
+    expect.fail('TODO')
+  })
+})

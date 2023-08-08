8f06956154d430131886ab5e16af88cdb600af1b
Edoardo Barbieri <edoardo@twinlogix.com>
2023-08-08 08:48:08 +0200

    fix: benchmark

--- packages/model/tests/benchmarks/converter.bench.ts
+++ packages/model/tests/benchmarks/converter.bench.ts
@@ -1,14 +1,13 @@
-import m, { Infer, decode, encode, getArbitrary, validate } from '../../src'
-import { test } from '@fast-check/vitest'
+import m, { arbitrary, decoder, encoder, types, validator } from '../../src'
 import { fc as gen } from '@fast-check/vitest'
 import prand from 'pure-rand'
-import { expect, describe, bench } from 'vitest'
+import { describe, bench } from 'vitest'
 
 const User = () =>
   m.object({
     username: m.string({ minLength: 3, maxLength: 30 }),
     email: m.string().nullable(),
-    points: m.integer({ minimum: [0, 'inclusive'], maximum: [Number.MAX_SAFE_INTEGER, 'exclusive'] }),
+    points: m.integer({ minimum: 0, exclusiveMaximum: Number.MAX_SAFE_INTEGER }),
     referredBy: m.optional(User),
     posts: m.array(Post),
   })
@@ -19,31 +18,32 @@ const Post = () =>
     owner: User,
     likes: m.array(User),
   })
-const arbitrary = getArbitrary(User, 3)
+const generator = arbitrary.fromType(User, {}, 3)
 const random = new gen.Random(prand.xoroshiro128plus(0))
-const values: Infer<typeof User>[] = []
+const values: types.Infer<typeof User>[] = []
 const encodedValues: unknown[] = []
-let size = 0
-for (let i = 0; i < 50000; i++) {
-  const value = arbitrary.generate(random, undefined).value
+
+for (let i = 0; i < 5000; i++) {
+  const value = generator.generate(random, undefined).value
   values.push(value)
-  encodedValues.push(encode(User, value))
+  encodedValues.push(encoder.encode(User, value))
 }
 
 describe('benchmark', () => {
-  bench('validate', () => {
+  bench('[ACTUAL] validate', () => {
     for (let i = 0; i < values.length; i++) {
-      validate(User, values[i])
+      validator.validate(User, values[i])
     }
   })
-  bench('encode', () => {
+
+  bench('[ACTUAL] decode', () => {
     for (let i = 0; i < values.length; i++) {
-      encode(User, values[i])
+      decoder.decode(User, encodedValues[i])
     }
   })
-  bench('decode', () => {
+  bench('[ACTUAL] encode', () => {
     for (let i = 0; i < values.length; i++) {
-      decode(User, encodedValues[i])
+      encoder.encode(User, values[i])
     }
   })
 })

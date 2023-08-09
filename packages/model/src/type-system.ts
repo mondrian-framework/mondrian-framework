bea5a83138b6a7e566d59a756a63da6046bea20c
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-08-09 11:23:37 +0200

    feat: remove multipleOf and use isInteger number option

--- packages/model/src/type-system.ts
+++ packages/model/src/type-system.ts
@@ -180,7 +180,7 @@ export type NumberTypeOptions = BaseOptions & {
   readonly exclusiveMaximum?: number
   readonly minimum?: number
   readonly exclusiveMinimum?: number
-  readonly multipleOf?: number
+  readonly isInteger?: boolean
 }
 
 /**
@@ -441,10 +441,6 @@ export type CustomTypeOptions<AdditionalOptions> = BaseOptions & AdditionalOptio
  *          ```
  */
 export function number(options?: OptionsOf<NumberType>): NumberType {
-  if (options?.multipleOf && options.multipleOf <= 0) {
-    throw new Error('Invalid multipleOf for integer (must be > 0)')
-  }
-
   const minimum = options?.minimum
   const exclusiveMinimum = options?.exclusiveMinimum
   const maximum = options?.maximum
@@ -502,10 +498,7 @@ export function number(options?: OptionsOf<NumberType>): NumberType {
  *           ```
  */
 export function integer(options?: OptionsOf<NumberType>): NumberType {
-  if (options?.multipleOf && options.multipleOf % 1 !== 0) {
-    throw new Error('Invalid multipleOf for integer (must be integer)')
-  }
-  return number({ multipleOf: 1, ...options })
+  return number({ ...options, isInteger: true })
 }
 
 /**

8597072046af3efec4c5c7c3aaceb08c02aefb88
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-08-25 15:50:01 +0200

    feat: add encoding module

---
+++ packages/model/src/encoding.ts
@@ -0,0 +1,9 @@
+/**
+ * The options that can be used when encoding a type:
+ * - `sensitiveInformationStrategy` its possible values are:
+ *   - `"hide"`: any type marked as sensitive will be encoded as a null
+ *   - `"keep"`: will encode any sensitive type leaving it unchanged
+ */
+export type Options = {
+  sensitiveInformationStrategy?: 'hide' | 'keep'
+}
--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -1,6 +1,7 @@
 import * as m from './types-exports'
 
 export * as types from './types-exports'
+export * as encoding from './encoding'
 export * as decoding from './decoding'
 export * as validation from './validation'
 export * as projection from './projection'

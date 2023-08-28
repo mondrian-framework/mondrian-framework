590b7853d02c622115401720c5dfbdf02e8355b9
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-08-28 17:05:03 +0200

    fix: make sure generators never generate objects with `valueOf` and `__proto__` fields

--- packages/model/tests/generic-properties.test.ts
+++ packages/model/tests/generic-properties.test.ts
@@ -1,4 +1,4 @@
-import { arbitrary, types } from '../src'
+import { arbitrary, decoding, types, validation } from '../src'
 import { assertOk } from './testing-utils'
 import { test } from '@fast-check/vitest'
 import { describe, expect } from 'vitest'
@@ -23,11 +23,17 @@ describe.concurrent('encoding', () => {
   // the decoded result would be undefined (and not the original null)
   test.prop([typeAndEncodedValue])('is the inverse of decoding', ([type, encoded]) => {
     //encoding(decoding(x)) = x
-    const decoded = assertOk(types.concretise(type).decode(encoded))
-    const encodedAgain = assertOk(types.concretise(type).encode(decoded as never))
+    const decoded = assertOk(types.concretise(type).decode(encoded), prettyErrors)
+    const encodedAgain = assertOk(types.concretise(type).encode(decoded as never), prettyErrors)
     expect(encodedAgain).toEqual(encoded)
   })
 })
+
+function prettyErrors(errors: decoding.Error[] | validation.Error[]): string {
+  return errors
+    .map((error) => ('assertion' in error ? validation.errorToString(error) : decoding.errorToString(error)))
+    .join('\n')
+}
 
 describe.concurrent('validation', () => {
   test.prop([arbitrary.typeAndValue()])('always succeeds on generated valid values', ([type, value]) => {

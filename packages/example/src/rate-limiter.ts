87ccf40c97492da12bdad5405c359bf5f3096a90
Edoardo Barbieri <edoardo@twinlogix.com>
2023-09-28 12:47:47 +0200

    chore: format

--- packages/example/src/rate-limiter.ts
+++ packages/example/src/rate-limiter.ts
@@ -4,4 +4,4 @@ import { createClient } from '@redis/client'
 const redisClient = process.env.REDIS_URL ? createClient() : undefined
 redisClient?.on('error', (err) => console.log('Redis Client Error', err))
 redisClient?.connect()
-export const slotProvider: SlotProvider | undefined = redisClient && new RedisSlotProvider(redisClient) 
+export const slotProvider: SlotProvider | undefined = redisClient && new RedisSlotProvider(redisClient)

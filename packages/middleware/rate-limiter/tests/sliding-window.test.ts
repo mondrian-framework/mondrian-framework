3162e10c93f798f6f81e4c18143d2b65a5ca1824
Edoardo Barbieri <edoardo@twinlogix.com>
2023-09-29 15:06:58 +0200

    chore: add redis test in rate-limiter

--- packages/middleware/rate-limiter/tests/sliding-window.test.ts
+++ packages/middleware/rate-limiter/tests/sliding-window.test.ts
@@ -1,6 +1,8 @@
 import { InMemorySlotProvider } from '../src/implementation/in-memory'
+import { RedisSlotProvider } from '../src/implementation/redis'
 import { Rate } from '../src/rate'
 import { SlidingWindow } from '../src/sliding-window'
+import redis from 'redis-mock'
 import { describe, expect, test } from 'vitest'
 
 describe('Sliding window in memory', async () => {
@@ -65,3 +67,75 @@ describe('Sliding window in memory', async () => {
 })
 
 //TODO: redis provider test?
+describe('Sliding window redis', async () => {
+  const memory: Record<string, number> = { 'test::90': 0 }
+  const client = {
+    async incr(key: string): Promise<number | void> {
+      memory[key] = memory[key] ?? 0
+      memory[key] = memory[key] + 1
+      return memory[key]
+    },
+    async get(key: string): Promise<string | void | null> {
+      return memory[key]?.toString() ?? null
+    },
+    async expireAt(): Promise<void> {
+      return
+    },
+  }
+  const slotProvider = new RedisSlotProvider(client as any, 'test')
+  test('rate limits', async () => {
+    let now = new Date(100000)
+    const window = new SlidingWindow({
+      rate: new Rate({ requests: 10, period: 30, scale: 'second' }),
+      slotProvider,
+      key: '',
+    })
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+    await delay(1)
+    now = new Date(120000)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+    await delay(1)
+    now = new Date(121000)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+    await delay(1)
+    now = new Date(122900)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+    await delay(1)
+    now = new Date(123010)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    now = new Date(150000)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    expect(window.isRateLimited(now)).toBe('allowed')
+    await delay(1)
+    expect(window.isRateLimited(now)).toBe('rate-limited')
+  })
+})
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}

755bbddcc846930af3786f1f28dcdbe78a3c7866
Mattia Minotti <mattia@twinlogix.com>
2023-11-09 13:03:56 +0100

    docs: homepahe enhancements

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -70,7 +70,7 @@ export default function Home(): JSX.Element {
                   automatic translation features to major standards: JSONSchema (OpenAPI), GraphQL and Protobuf.
                 </div>
                 <div className={styles.featureButton}>
-                  <Link className={`button button--primary button--lg`} to="/docs/docs/introduction">
+                  <Link className={`button button--primary button--lg`} to="/docs/docs/model">
                     Learn more about data model
                   </Link>
                 </div>
@@ -101,15 +101,92 @@ export type User = model.Infer<typeof User>
             </div>
           </div>
         </section>
-
-        {/*  
         <section>
           <div className="container">
             <div className={styles.feature}>
+              <div className={styles.featureColumn}>
+                <div className={styles.featureSubtitle}>@mondrian-framework/module</div>
+                <div className={styles.featureTitle}>
+                  <em>Modularity</em> and well defined boundaries
+                </div>
+                <div className={styles.featureDescription}>
+                  Define your system as a set of functions grouped into modules. Each function has a single responsibility, with well-defined boundaries based on a formal interface, with no direct dependencies on the execution environment. This maximizes reuse and allows the system as a whole to evolve technologically without requiring rewriting application logic.
+                </div>
+                <div className={styles.featureButton}>
+                  <Link className={`button button--primary button--lg`} to="/docs/docs/module">
+                    Learn more about modules and functions
+                  </Link>
+                </div>
+              </div>
+              <div className={styles.featureColumn}>
+                <CodeBlock language="ts" title="module.ts" showLineNumbers>
+                  {`import { functions } from '@mondrian-framework/module'
+import { model } from '@mondrian-framework/model'
+
+const register = functions.build({
+  input: model.object({ 
+    email: model.email(), 
+    password: model.string() 
+  }),
+  output: model.object({ jwt: model.string() }),
+  errors: { weakPassword: model.string() },
+  async body({ input: { email, password } }) {
+    // BUSINESS LOGIC
+  },
+})
+
+const userModule = module.build({
+  name: 'User',
+  version: '0.0.1',
+  functions: { register }
+})
+`}
+                </CodeBlock>
+              </div>
             </div>
-        </div>
+          </div>
         </section>
-        */}
+        <section>
+          <div className="container">
+            <div className={styles.feature}>
+              <div className={styles.featureColumn}>
+                <div className={styles.featureSubtitle}>@mondrian-framework/runtime</div>
+                <div className={styles.featureTitle}>
+                  Multiple <em>runtimes</em> with zero effort
+                </div>
+                <div className={styles.featureDescription}>
+                  Provide your own functions in any way you like (as a REST API, GraphQL, gRPC, reactive to an SQS queue or a Kafka topic, etc.).  You can also develop your own fully customized runtime based on your needs. Every framework runtime also support all best practices, from observability via Open Telemetry to automated documentation.
+                </div>
+                <div className={styles.featureButton}>
+                  <Link className={`button button--primary button--lg`} to="/docs/docs/runtime">
+                    Learn more about runtimes
+                  </Link>
+                </div>
+              </div>
+              <div className={styles.featureColumn}>
+                <CodeBlock language="ts" title="model.ts" showLineNumbers>
+                  {`
+import { graphql } from '@mondrian-framework/graphql'
+import { serve } from '@mondrian-framework/graphql-fastify'
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+const api = graphql.build({ 
+  module: userModule,
+  options: { introspection: true },
+})
+serve({ server, api })
+
+server.listen({ port: 4000 }).then((address) => {
+  console.log(\`Server started at address \${address}/graphql\`)
+})
+`}
+                </CodeBlock>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   )

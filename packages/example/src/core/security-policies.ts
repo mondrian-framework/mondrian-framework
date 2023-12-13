f2e87ba2ffc05206f9d47424a41bd639bc248bb6
Edoardo Barbieri <edoardo@twinlogix.com>
2023-12-13 17:33:16 +0100

    fix: build error

--- packages/example/src/core/security-policies.ts
+++ packages/example/src/core/security-policies.ts
@@ -7,34 +7,30 @@ export const loggedUser: (userId: number) => security.Policies = (userId) =>
   security
 
     .on(User)
-    .allows([
-      {
-        selection: true,
-        restriction: { id: { equals: userId } },
-      },
-      {
-        selection: { id: true, firstName: true, lastName: true },
-      },
-    ])
+    .allows({
+      selection: true,
+      restriction: { id: { equals: userId } },
+    })
+    .allows({
+      selection: { id: true, firstName: true, lastName: true },
+    })
 
     .on(Post)
-    .allows([
-      {
-        selection: true,
-        filter: { visibility: { equals: 'PRIVATE' }, author: { id: { equals: userId } } },
+    .allows({
+      selection: true,
+      filter: { visibility: { equals: 'PRIVATE' }, author: { id: { equals: userId } } },
+    })
+    .allows({
+      selection: true,
+      filter: {
+        visibility: { equals: 'FOLLOWERS' },
+        author: { followers: { some: { follower: { id: { equals: userId } } } } },
       },
-      {
-        selection: true,
-        filter: {
-          visibility: { equals: 'FOLLOWERS' },
-          author: { followers: { some: { follower: { id: { equals: userId } } } } },
-        },
-      },
-      {
-        selection: true,
-        filter: { visibility: { equals: 'PUBLIC' } },
-      },
-    ])
+    })
+    .allows({
+      selection: true,
+      filter: { visibility: { equals: 'PUBLIC' } },
+    })
 
     .on(Like)
     .allows({ selection: true })

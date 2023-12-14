1ffb44fe2b34b6f01b7d33174388a32468ecaab3
Edoardo Barbieri <edoardo@twinlogix.com>
2023-12-14 20:57:55 +0100

    chore: add some comments on security policies example

--- packages/example/src/core/security-policies.ts
+++ packages/example/src/core/security-policies.ts
@@ -1,21 +1,26 @@
 import { Like, Post } from './post'
 import { Follower, MyUser, User } from './user'
-import { model } from '@mondrian-framework/model'
 import { security } from '@mondrian-framework/module'
 
 export const loggedUser: (userId: number) => security.Policies = (userId) =>
   security
-
+    //ONLY FOR LOGGED-IN USERS
     .on(User)
+    //Can read all fields if it's getting my user
     .allows({
       selection: true,
       restriction: { id: { equals: userId } },
     })
+    //otherwise can read only this fields
     .allows({
       selection: { id: true, firstName: true, lastName: true },
     })
 
     .on(Post)
+    //Can read all fields on post any post but with a given set of filters
+    // - can read private posts only if author is me
+    // - can read "followrs" posts only if i'm a follower of the author
+    // - can read public post, always
     .allows({
       selection: true,
       filter: { visibility: { equals: 'PRIVATE' }, author: { id: { equals: userId } } },
@@ -32,16 +37,18 @@ export const loggedUser: (userId: number) => security.Policies = (userId) =>
       filter: { visibility: { equals: 'PUBLIC' } },
     })
 
+    //This is needed to traverse the graph
     .on(Like)
     .allows({ selection: true })
-
     .on(Follower)
     .allows({ selection: true })
 
 export const guest: security.Policies = security
 
+  //This is a wrap of User type that is returned only on register phase, and i can read anything
   .on(MyUser)
   .allows({ selection: true })
 
+  //On other users i can read only the firstName as a guest
   .on(User)
   .allows({ selection: { firstName: true } })

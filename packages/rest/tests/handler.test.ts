36d70ba408b986d6751fcd9b4b96da0c12713569
Edoardo Barbieri <edoardo@twinlogix.com>
2024-01-08 12:03:13 +0100

    fix: tests

--- packages/rest/tests/handler.test.ts
+++ packages/rest/tests/handler.test.ts
@@ -130,28 +130,28 @@ describe('rest handler', () => {
     const handler = buildHandler('f0', { method: 'get' })
     const response = await handler({})
     expect(response.status).toBe(200)
-    expect(response.body).toBe(1)
+    expect(response.body).toBe(JSON.stringify(1))
   })
 
   test('works on [scalar input on query]', async () => {
     const handler = buildHandler('f1', { method: 'get' })
     const response = await handler({ query: { input: '123' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(123)
+    expect(response.body).toBe(JSON.stringify(123))
   })
 
   test('works on [scalar input on path]', async () => {
     const handler = buildHandler('f1', { method: 'get', path: '/f1/{value}' })
     const response = await handler({ params: { value: '123' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(123)
+    expect(response.body).toBe(JSON.stringify(123))
   })
 
   test('works on [scalar input on body]', async () => {
     const handler = buildHandler('f1', { method: 'post', path: '/f1' })
     const response = await handler({ body: '123' })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(123)
+    expect(response.body).toBe(JSON.stringify(123))
   })
 
   test('works on [scalar input on custom]', async () => {
@@ -165,89 +165,93 @@ describe('rest handler', () => {
     })
     const response = await handler({ headers: { value: '123' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(123)
+    expect(response.body).toBe(JSON.stringify(123))
   })
 
   test('dont works on [scalar input on query]', async () => {
     const handler = buildHandler('f1', { method: 'get' })
     const response = await handler({})
     expect(response.status).toBe(400)
-    expect(response.body).toStrictEqual({
-      errors: [
-        {
-          expected: 'string',
-          got: undefined,
-          path: '$',
-        },
-      ],
-      message: 'Invalid input',
-    })
+    expect(response.body).toStrictEqual(
+      JSON.stringify({
+        errors: [
+          {
+            expected: 'string',
+            got: undefined,
+            path: '$',
+          },
+        ],
+        message: 'Invalid input',
+      }),
+    )
   })
 
   test('works on [object input on query]', async () => {
     const handler = buildHandler('f2', { method: 'get', path: '/f2' })
     const response = await handler({ query: { a: '1.1', b: '2' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(2.2)
+    expect(response.body).toBe(JSON.stringify(2.2))
   })
 
   test('works on [object input on partially query and partially params]', async () => {
     const handler = buildHandler('f2', { method: 'get', path: '/f2/{a}' })
     const response = await handler({ query: { b: '2' }, params: { a: '1.1' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(2.2)
+    expect(response.body).toBe(JSON.stringify(2.2))
   })
 
   test('works on [object input on params]', async () => {
     const handler = buildHandler('f2', { method: 'get', path: '/f2/{b}/{a}' })
     const response = await handler({ params: { a: '1.1', b: '2' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(2.2)
+    expect(response.body).toBe(JSON.stringify(2.2))
   })
 
   test('works on [object input on body]', async () => {
     const handler = buildHandler('f2', { method: 'post', path: '/f2' })
     const response = await handler({ body: { a: '1.1', b: 2 } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(2.2)
+    expect(response.body).toBe(JSON.stringify(2.2))
   })
 
   test('works on [object input partially on body and partially on params]', async () => {
     const handler = buildHandler('f2', { method: 'post', path: '/f2/{b}' })
     const response = await handler({ body: { a: 1.1, b: '2' }, params: { b: '2' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe(2.2)
+    expect(response.body).toBe(JSON.stringify(2.2))
   })
 
   test('dont works on [object input on body]', async () => {
     const handler = buildHandler('f2', { method: 'post', path: '/f2' })
     const response = await handler({ body: { a: {}, b: 1 } })
     expect(response.status).toBe(400)
-    expect(response.body).toStrictEqual({
-      errors: [{ expected: 'number', got: {}, path: '$.a' }],
-      message: 'Invalid input',
-    })
+    expect(response.body).toStrictEqual(
+      JSON.stringify({
+        errors: [{ expected: 'number', got: {}, path: '$.a' }],
+        message: 'Invalid input',
+      }),
+    )
   })
 
   test('works on [object input spread on param]', async () => {
     const handler = buildHandler('f3', { method: 'get', path: '/f3/{ping}' })
     const response = await handler({ params: { ping: 'ping' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe('pong')
+    expect(response.body).toBe(JSON.stringify('pong'))
   })
 
   test('dont works on [object input spread on param]', async () => {
     const handler = buildHandler('f3', { method: 'post', path: '/f3/{ping}', errorCodes: { notAPing: 401 } })
     const response = await handler({ params: { ping: 'lol' } })
     expect(response.status).toBe(401)
-    expect(response.body).toStrictEqual({ notAPing: 'Not a ping!' })
+    expect(response.body).toStrictEqual(JSON.stringify({ notAPing: 'Not a ping!' }))
   })
 
   test('works on [object input on query]', async () => {
     const handler = buildHandler('f3', { method: 'get', path: '/f3' })
     const response = await handler({ query: { ping: 'ping' } })
     expect(response.status).toBe(200)
-    expect(response.body).toBe('pong')
+    expect(response.body).toBe(JSON.stringify('pong'))
   })
 
   test('dont works on [object input spread on param 2]', async () => {
@@ -281,14 +285,16 @@ describe('rest handler', () => {
     const handler = buildHandler('f5', { method: 'get', path: '/f5' })
     const response = await handler({ query: { input: '' } })
     expect(response.status).toBe(200)
-    expect(response.body).toStrictEqual({ live: true, username: 'name' })
+    expect(response.body).toStrictEqual(JSON.stringify({ username: 'name', live: true }))
 
     const response2 = await handler({
       query: { input: '' },
       headers: { retrieve: JSON.stringify({ select: { friend: true } }) },
     })
     expect(response2.status).toBe(200)
-    expect(response2.body).toStrictEqual({ live: true, username: 'name', friend: { live: true, username: 'name2' } })
+    expect(response2.body).toStrictEqual(
+      JSON.stringify({ username: 'name', live: true, friend: { username: 'name2', live: true } }),
+    )
   })
 
   test('dont works on [output with retrieve]', async () => {
@@ -303,15 +309,17 @@ describe('rest handler', () => {
       headers: { retrieve: JSON.stringify({ select: { friends: true } }) },
     })
     expect(response2.status).toBe(400)
-    expect(response2.body).toStrictEqual({
-      errors: [{ expected: 'undefined', got: true, path: '$.select.friends' }],
-      message: 'Invalid retrieve',
-    })
+    expect(response2.body).toStrictEqual(
+      JSON.stringify({
+        errors: [{ expected: 'undefined', got: true, path: '$.select.friends' }],
+        message: 'Invalid retrieve',
+      }),
+    )
   })
   test('works on [complex object input on query]', async () => {
     const handler = buildHandler('f6', { method: 'get', path: '/f6' })
     const response = await handler({ query: { a: '1', 'b[a]': '2', 'b[b]': '3' } })
     expect(response.status).toBe(200)
-    expect(response.body).toStrictEqual(6)
+    expect(response.body).toStrictEqual(JSON.stringify(6))
   })
 })

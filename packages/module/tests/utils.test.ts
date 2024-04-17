3e6e253ae920c0f7d957777a11731b6f976b5818
Edoardo Barbieri <edoardo@twinlogix.com>
2024-04-17 14:52:31 +0200

    chore: add regression test

--- packages/module/tests/utils.test.ts
+++ packages/module/tests/utils.test.ts
@@ -2,12 +2,14 @@ import { hasNestedPromises, reolsveNestedPromises } from '../src/utils'
 import { expect, test } from 'vitest'
 
 test('reolsveNestedPromises', async () => {
+  const now = new Date()
   const r1 = await reolsveNestedPromises({
     a: Promise.resolve(1),
     b: Promise.resolve([Promise.resolve(2), Promise.resolve(3)]),
     c: Promise.resolve({ d: Promise.resolve(4), e: Promise.resolve(5) }),
+    d: now
   })
-  expect(r1).toEqual({ a: 1, b: [2, 3], c: { d: 4, e: 5 } })
+  expect(r1).toEqual({ a: 1, b: [2, 3], c: { d: 4, e: 5 }, d: now })
 
   const r2 = await reolsveNestedPromises(Promise.resolve(1))
   expect(r2).toEqual(1)

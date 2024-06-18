bafcfcc1bda9fe9a7954b35a69ed24f166091114
Edoardo Barbieri <edoardo@twinlogix.com>
2024-06-18 15:43:43 +0200

    feat: implement concurrency in sqs handler

--- packages/aws-sqs/src/listener.ts
+++ packages/aws-sqs/src/listener.ts
@@ -1,7 +1,6 @@
 import { Api, FunctionSpecifications } from './api'
 import * as AWS from '@aws-sdk/client-sqs'
-import { model } from '@mondrian-framework/model'
-import { functions, logger, module } from '@mondrian-framework/module'
+import { exception, functions, logger, module } from '@mondrian-framework/module'
 import { sleep } from '@mondrian-framework/utils'
 
 /**
@@ -17,7 +16,7 @@ export function listen<Fs extends functions.FunctionImplementations>({
   const client: AWS.SQS = new AWS.SQS(api.options?.config ?? {})
   const promises: Promise<void>[] = []
   const alive: { yes: boolean } = { yes: true }
-  for (const functionName of Object.keys(api.module.functions.definitions)) {
+  for (const functionName of Object.keys(api.module.functions)) {
     const specifications = api.functions[functionName]
     if (!specifications) {
       continue
@@ -48,16 +47,7 @@ export function listen<Fs extends functions.FunctionImplementations>({
   }
 }
 
-async function listenForMessage<Fs extends functions.FunctionImplementations>({
-  alive,
-  queueUrl,
-  client,
-  module,
-  functionName,
-  context,
-  specifications,
-  concurrency,
-}: {
+type ListenForMessageInput<Fs extends functions.FunctionImplementations> = {
   queueUrl: string
   alive: { yes: boolean }
   client: AWS.SQS
@@ -66,43 +56,80 @@ async function listenForMessage<Fs extends functions.FunctionImplementations>({
   context: (args: { message: AWS.Message }) => Promise<module.FunctionsToContextInput<Fs>>
   specifications: FunctionSpecifications
   concurrency: number
-}) {
-  const functionBody = module.functions[functionName]
+}
+
+async function listenForMessage<Fs extends functions.FunctionImplementations>({
+  alive,
+  queueUrl,
+  client,
+  concurrency,
+  ...input
+}: ListenForMessageInput<Fs>) {
+  let slots = concurrency
+  while (alive.yes) {
+    try {
+      const message = await client.receiveMessage({ QueueUrl: queueUrl, MaxNumberOfMessages: 1, WaitTimeSeconds: 20 })
+      slots++
+      handleMessages({ ...input, messages: message.Messages, client, queueUrl })
+        .then(() => {
+          slots--
+        })
+        .catch(() => {
+          slots--
+        })
+    } catch (error) {
+      do {
+        await sleep(1000)
+      } while (slots <= 0)
+    }
+  }
+}
+
+async function handleMessages<Fs extends functions.FunctionImplementations>({
+  queueUrl,
+  client,
+  module,
+  context,
+  specifications,
+  functionName,
+  messages,
+}: Omit<ListenForMessageInput<Fs>, 'alive' | 'concurrency'> & { messages: AWS.Message[] | undefined }) {
   const baseLogger = logger.build({
     moduleName: module.name,
     operationType: queueUrl,
     operationName: functionName,
     server: 'SQS',
   })
-  while (alive.yes) {
-    try {
-      const message = await client.receiveMessage({ QueueUrl: queueUrl, MaxNumberOfMessages: 1, WaitTimeSeconds: 20 })
-      if (!message.Messages || message.Messages.length !== 1) {
-        continue
-      }
-      //TODO [Good first issue]: execute in a separate handler (concurrency)
-      const m = message.Messages[0]
-      let body: unknown
-      try {
-        body = m.Body === undefined ? undefined : JSON.parse(m.Body)
-      } catch {
-        if (specifications.malformedMessagePolicy === 'delete') {
-          await client.deleteMessage({ QueueUrl: queueUrl, ReceiptHandle: m.ReceiptHandle })
-        }
-        continue
-      }
-      const contextInput = await context({ message: m })
-      await functionBody.rawApply({
-        rawInput: body,
-        rawRetrieve: {},
-        //tracer: functionBody.tracer, //TODO: add opentelemetry istrumentation
-        contextInput: contextInput as Record<string, unknown>,
-        logger: baseLogger,
-        decodingOptions: { typeCastingStrategy: 'tryCasting', ...module.options?.preferredDecodingOptions },
-      })
+  const functionBody = module.functions[functionName]
+  if (!messages || messages.length !== 1) {
+    return
+  }
+  const m = messages[0]
+  let body: unknown
+  try {
+    body = m.Body === undefined ? undefined : JSON.parse(m.Body)
+  } catch {
+    if (specifications.malformedMessagePolicy === 'delete') {
       await client.deleteMessage({ QueueUrl: queueUrl, ReceiptHandle: m.ReceiptHandle })
-    } catch (error) {
-      await sleep(1000)
+    }
+    return
+  }
+  const contextInput = await context({ message: m })
+  try {
+    await functionBody.rawApply({
+      rawInput: body,
+      rawRetrieve: {},
+      //tracer: functionBody.tracer, //TODO: add opentelemetry istrumentation
+      contextInput: contextInput as Record<string, unknown>,
+      logger: baseLogger,
+      decodingOptions: { typeCastingStrategy: 'tryCasting', ...module.options?.preferredDecodingOptions },
+    })
+  } catch (e) {
+    if (e instanceof exception.InvalidInput && specifications.malformedMessagePolicy === 'delete') {
+      await client.deleteMessage({ QueueUrl: queueUrl, ReceiptHandle: m.ReceiptHandle })
+    } else {
+      throw e
     }
   }
+  await client.deleteMessage({ QueueUrl: queueUrl, ReceiptHandle: m.ReceiptHandle })
 }

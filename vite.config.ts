3ea81dcb1b80adcc15f9ece313b6969c6fc1f524
Edoardo Barbieri <edoardo@twinlogix.com>
2025-12-17 14:42:39 +0100

    feat: set test timeout to 10000ms in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    testTimeout: 10000,
     coverage: {
       exclude: ['packages/example/**', 'packages/docs/**'],
       include: ['packages/*/src/**'],
